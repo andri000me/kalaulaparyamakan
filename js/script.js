b3b33d9242ec94e5efedc7b1e6d3498316aa36aa
farkhan432 <73373803+farkhan432@users.noreply.github.com>
2020-10-24 11:25:51 +0700

    upload file kalaulaparyamakan
    upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const txtElement = ['Makanan', 'Minuman', 'Cemilan'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if (count == txtElement.length) {
+		count = 0;
+	}
+	
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if (words.length == currentTxt.length) {
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);
+
+	})();
